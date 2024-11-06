99e8ef8368378d4971da70225cf148c5cca8c7a4
prit chhatrala <pritchhatrala3333@gmail.com>
2024-11-06 14:08:34 +0530

    new practicals

--- MODULE 3.2 (C Language Programing with C)/practical3.c
+++ MODULE 3.2 (C Language Programing with C)/practical3.c
@@ -10,16 +10,4 @@ int main() {
     (num % 2 == 0) ? printf("%d is even.\n", num) : printf("%d is odd.\n", num);
 
 }
-=======
-#include <stdio.h>
 
-int main() {
-    int num;
-
-    printf("Enter a number: ");
-    scanf("%d", &num);
-
-    (num % 2 == 0) ? printf("%d is even.\n", num) : printf("%d is odd.\n", num);
-
-}
->>>>>>> 9372596b8d8a5284dc08a9a11ab0da7b531014c4:assignment/MODULE 3.2 (C Language Programing with C)/practical3.c
--- MODULE 3.3 (File Handling and Debugging)/practical2.c
+++ MODULE 3.3 (File Handling and Debugging)/practical2.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:MODULE 3.3 (File Handling and Debugging)/practical2.c
 #include <stdio.h>
 
 int main() {
@@ -42,48 +41,3 @@ int main() {
     }
 
 }
-=======
-#include <stdio.h>
-
-int main() {
-    int choice;
-    float num1, num2, result;
-
-    printf("Calculator Menu:\n");
-    printf("1. Addition (+)\n");
-    printf("2. Subtraction (-)\n");
-    printf("3. Multiplication (*)\n");
-    printf("4. Division (/)\n");
-    printf("Enter your choice (1-4): ");
-    scanf("%d", &choice);
-
-    printf("Enter two numbers: ");
-    scanf("%f %f", &num1, &num2);
-
-    switch (choice) {
-        case 1:
-            result = num1 + num2;
-            printf("Result of Addition: %.2f\n", result);
-            break;
-        case 2:
-            result = num1 - num2;
-            printf("Result of Subtraction: %.2f\n", result);
-            break;
-        case 3:
-            result = num1 * num2;
-            printf("Result of Multiplication: %.2f\n", result);
-            break;
-        case 4:
-            if (num2 != 0) {
-                result = num1 / num2;
-                printf("Result of Division: %.2f\n", result);
-            } else {
-                printf("Error: Division by zero is not allowed.\n");
-            }
-            break;
-        default:
-            printf("Invalid choice! Please enter a valid option (1-4).\n");
-    }
-
-}
->>>>>>> 9372596b8d8a5284dc08a9a11ab0da7b531014c4:assignment/MODULE 3.3 (File Handling and Debugging)/practical2.c
