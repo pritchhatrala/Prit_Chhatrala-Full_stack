1d70a218a40fda71619820095da4550ba2226014
prit chhatrala <153066916+pritchhatrala@users.noreply.github.com>
2024-11-17 19:00:40 +0530

    Add files via upload

---
+++ Assignment/MODULE 3.2 (C Language Programing with C)/practical2.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+int main() {
+    int a;
+    int b;
+
+    printf("Enter first numbers: ");
+    scanf("%d", &a);
+
+    printf("Enter second numbers: ");
+    scanf("%d", &b);
+    printf("Before swapping:\na = %d,\nb = %d", a, b);
+
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    printf("\nAfter swapping:\na = %d,\nb = %d", a, b);
+
+}
---
+++ Assignment/MODULE 3.3 (File Handling and Debugging)/practical1.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+int findMaxnum(int arr[], int size) {
+    int max = arr[0];  
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > max) {
+            max = arr[i];  
+        }
+    }
+    return max;
+}
+
+int main() {
+    int size;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    int arr[size];
+
+    printf("Enter %d elements of the array:\n", size);
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int max = findMaxnum(arr, size);
+
+    printf("The maximum number in the array is: %d\n", max);
+
+}
+<<<<<<< HEAD:MODULE 3.3 (File Handling and Debugging)/practical1.c
+=======
+=======
+#include <stdio.h>
+
+int findMaxnum(int arr[], int size) {
+    int max = arr[0];  
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > max) {
+            max = arr[i];  
+        }
+    }
+    return max;
+}
+
+int main() {
+    int size;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    int arr[size];
+
+    printf("Enter %d elements of the array:\n", size);
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int max = findMaxnum(arr, size);
+
+    printf("The maximum number in the array is: %d\n", max);
+
+    return 0;
+}
+>>>>>>> 9372596b8d8a5284dc08a9a11ab0da7b531014c4:assignment/MODULE 3.3 (File Handling and Debugging)/practical1.c
+>>>>>>> 88c6a4f9f46e210385f966a3c869bc915477bd67:Assignment/MODULE 3.3 (File Handling and Debugging)/practical1.c
